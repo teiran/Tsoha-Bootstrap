7688c11eba9fc6affec1aefe5b32e6f610e91b08
teiran <tiera.hirvonen@gmail.com>
2017-03-16 18:29:05 +0200

    dokumentaatio

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="tierahir"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tierahir.users.cs.helsinki.fi/titolabra2"
+PROJECT_FOLDER="titolabra2"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
