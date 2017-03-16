b1152e1de613740ce27386c9642a332ded5e8883
teiran <tiera.hirvonen@gmail.com>
2017-03-16 17:40:17 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="tierahir"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="tierahir.users.cs.helsinki.fi/titolabra2"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
